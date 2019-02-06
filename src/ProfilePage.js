9b698dab932878c75f046bd3b662dbbc2b849391
chompusama <chadaporn.p@kkumai.com>
2019-02-06 22:54:23 +0700

    BookmarkPage, ProfilePage and img

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Button, TouchableHighlight, StyleSheet, Text, View, ImageBackground, Image, TouchableWithoutFeedback } from 'react-native';
 
 import { Color } from './config/color';
+import firebase from './config/firebase';
+
 
 export default class App extends Component {
     // static navigationOptions = () => ({
@@ -18,11 +20,33 @@ export default class App extends Component {
     //         <Text>6</Text>
     // });
 
+    constructor(props) {
+        super(props);
+        this.state={
+            profileURI : firebase.auth().currentUser.photoURLk,
+            displayName : firebase.auth().currentUser.displayName
+        }
+    }
+
     render() {
         return (
-            <View>
-                <Text>Profile Page</Text>
+            <View style={{flex: 1, flexDirection: 'column', justifyContent: 'flex-start' }}>
+                <View style={{ flex: 1.5 }}>
+                    <ImageBackground source={require('../img/profile-bg.jpg')} style={{flex: 1, width: '100%', flexDirection: 'column',justifyContent: 'center'}}>
+                        <View>
+                            <Image source={{ uri: profileURI }} style={{height: 45, width: 45, borderRadius: 40}} />
+                            <Text style={{fontSize: 15}}>displayName</Text>
+                        </View>
+                    </ImageBackground>
+                </View>
+                <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start', backgroundColor: 'white' }}>
+                    
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+
+});
